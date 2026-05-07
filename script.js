77e165e5de408e58eebb3dc410fca28927b992aa
Mordor828 <tranm98@gmail.com>
2026-05-07 18:23:18 +0200

    #script work

--- script.js
+++ script.js
@@ -3,5 +3,8 @@ cards.forEach((card) => {
   card.onmousemove = function (e) {
     let x = e.pageX - card.offsetLeft;
     let y = y.pageY - card.offsetTop;
+
+    card.style.setProperty("--x", x + "px");
+    card.style.setProperty("--y", y + "px");
   };
 });
