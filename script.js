82418d34343ffaf4ffece0342f86fa93e3cf0a2f
Mordor828 <tranm98@gmail.com>
2026-05-08 22:34:57 +0200

    #

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let card = document.querySelector(".card");
+let cards = document.querySelector(".card");
 cards.forEach((card) => {
   card.onmousemove = function (e) {
     let x = e.pageX - card.offsetLeft;
